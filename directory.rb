e2baa4cbc414c7014db90076e0c5460aed00781f
Noel Vock <noel1uk@gmail.com>
2018-01-21 10:45:56 +0000

    Made cohort dependent on user input and implemented some user input checks

--- directory.rb
+++ directory.rb
@@ -1,20 +1,52 @@
 # First get the width of the screen
 require 'io/console'
+# Create the array of possible cohorts
+cohorts = ['January',
+           'February',
+           'March',
+           'April',
+           'May',
+           'June',
+           'July',
+           'August',
+           'September',
+           'October',
+           'November',
+           'December']
 # let's put all the students into an array
-def input_students
+def input_students(cohorts)
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
+  puts "Now enter the students cohort"
+  # Check and sort user input
+  cohort = gets.chomp.downcase.capitalize
+  while !cohorts.include?(cohort)
+    puts 'You have entered an invalid cohort name. Enter the cohorts month'
+    cohort = gets.chomp.downcase.capitalize
+  end
+  cohort = cohort.to_sym
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort}
+    puts "\nNow we have #{students.count} students.
+Enter the name of the next student"
     # get another name from the user
     name = gets.chomp
+    if name == ''
+      break
+    end
+    puts "Enter the students cohort"
+    cohort = gets.chomp.downcase.capitalize
+    while !cohorts.include?(cohort)
+      puts 'You have entered an invalid cohort name. Enter the cohorts month'
+      cohort = gets.chomp.downcase.capitalize
+    end
+    cohort = cohort.to_sym
   end
   # return the array of students
   students
@@ -32,7 +64,8 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 #nothing happens until we call the methods
-students = input_students()
+students = input_students(cohorts)
 print_header
 print(students)
 print_footer(students)
+p students
